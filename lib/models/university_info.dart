d6c3b02370ff386735da170c666a5dff9e137a9e
yesharish <yesharish@yahoo.in>
2022-01-19 22:52:24 +1100

    Static method to fetch all universities and return as List

--- lib/models/university_info.dart
+++ lib/models/university_info.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class UniversityInfo {
   String univName;
   String city;
@@ -25,4 +27,28 @@ class UniversityInfo {
       this.customField3,
       this.customField4,
       this.customField5});
+
+  UniversityInfo.fromJson(Map<String, dynamic> json)
+      : univName = json['univName'],
+        city = json['city'],
+        state = json['state'],
+        postcode = json['postcode'],
+        website = json['website'],
+        departments = json['departments'],
+        syllabusVersion = json['syllabusVersion'],
+        customField1 = json['customField1'],
+        customField2 = json['customField2'],
+        customField3 = json['customField3'],
+        customField4 = json['customField4'],
+        customField5 = json['customField5'];
+
+  static final FirebaseFirestore _fs = FirebaseFirestore.instance;
+
+  static Future<List<UniversityInfo>> fetchAllUnivs() async {
+    var _univCollection = await _fs.collection('universities').get();
+    var allUnivs = _univCollection.docs
+        .map((e) => UniversityInfo.fromJson(e.data()))
+        .toList();
+    return allUnivs;
+  }
 }
