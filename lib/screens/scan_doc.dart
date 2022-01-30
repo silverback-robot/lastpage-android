e64377d31b1f1c3d9f5b79db9bab341065e35c3f
yesharish <yesharish@yahoo.in>
2022-01-30 15:27:59 +1100

    Added ImagePicker and display captured image on screen

--- lib/screens/scan_doc.dart
+++ lib/screens/scan_doc.dart
@@ -1,15 +1,54 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class ScanDoc extends StatelessWidget {
+class ScanDoc extends StatefulWidget {
   const ScanDoc({Key? key}) : super(key: key);
+
+  @override
+  State<ScanDoc> createState() => _ScanDocState();
+}
+
+class _ScanDocState extends State<ScanDoc> {
+  final List<File> _capturedDocs = [];
+
+  void _captureDoc() async {
+    var picker = ImagePicker();
+    var capturedDoc = await picker.pickImage(
+        source: ImageSource.camera,
+        maxHeight: 1080,
+        preferredCameraDevice: CameraDevice.rear);
+    setState(() {
+      _capturedDocs.add(File(capturedDoc!.path));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text(
-          "DocScanner goes here!",
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(
+                  8,
+                ),
+                child: _capturedDocs.isNotEmpty
+                    ? Image.file(_capturedDocs.first)
+                    : const Text("Click a picture of the note to be saved."),
+              ),
+              ElevatedButton.icon(
+                onPressed: _captureDoc,
+                icon: const Icon(
+                  Icons.document_scanner,
+                ),
+                label: const Text(
+                  "Capture Document",
+                ),
+              )
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
