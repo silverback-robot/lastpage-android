8bc36fed09aa2ff893c80f410e716a74ae7c7de4
yesharish <yesharish@yahoo.in>
2022-02-02 08:52:57 +1100

    WIP: DocScanner API request/response

--- lib/models/notes.dart
+++ lib/models/notes.dart
@@ -1,4 +1,6 @@
 import 'dart:io';
+// import 'dart:async';
+import 'dart:typed_data';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
@@ -11,6 +13,7 @@ class Notes {
   final String _docScannerEndPoint = "/v1/docscanner";
 
   final List<File> allPages = [];
+  final List<File> scannedPages = [];
 
   Future<void> capturePage() async {
     var picker = ImagePicker();
@@ -21,13 +24,37 @@ class Notes {
     allPages.add(File(capturedDoc!.path));
   }
 
-  Future<void> docScanner() async {
+  Future<void> docScanner(String camImg) async {
     final String _authToken = await _auth.currentUser!.getIdToken();
 
     final docScannerOut = await http.post(
-        Uri.parse(_docScannerUrl + _docScannerEndPoint),
-        headers: {HttpHeaders.authorizationHeader: _authToken});
-    // TODO: Write downloaded image to a file and display on UI
+      Uri.parse(_docScannerUrl + _docScannerEndPoint),
+      headers: {HttpHeaders.authorizationHeader: _authToken},
+    );
+
+    var uri = Uri.parse(_docScannerUrl + _docScannerEndPoint);
+    var request = http.MultipartRequest('POST', uri)
+      ..files.add(await http.MultipartFile.fromPath('file', camImg));
+    var response = await request.send();
+
+    if (response.statusCode == 200) {
+      final Uint8List _scannedDocRaw = docScannerOut.bodyBytes;
+      final _scannedDoc =
+          await File("docScannerOut_001").writeAsBytes(_scannedDocRaw);
+      scannedPages.add(_scannedDoc);
+    }
+    // final ByteData imageData = await NetworkAssetBundle(
+    //         Uri.parse(_docScannerUrl + _docScannerEndPoint))
+    //     .load("");
+
+    // final ByteData imageData = (docScannerOut.body);
+
+    // final Uint8List bytes = imageData.buffer.asUint8List();
+    // var _tmpDir = await getTemporaryDirectory();
+    // final _scannedDocPath = "${_tmpDir.path}/";
+
+    // var _scannedDoc = MemoryImage(bytes);
+    // scannedPages.add(File(_scannedDoc));
   }
 }
 
