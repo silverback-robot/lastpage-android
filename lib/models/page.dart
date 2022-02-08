8ba8088f806193f3f39a63b09dfd44de723ebe02
yesharish <yesharish@yahoo.in>
2022-02-08 22:40:30 +1100

    Move DocScanner method from Pages to Page class

--- lib/models/page.dart
+++ lib/models/page.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:image/image.dart' as img;
+import 'dart:typed_data';
+import 'package:http/http.dart' as http;
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Page {
   File original;
@@ -63,5 +66,55 @@ class Page {
       _processedImg.writeAsBytes(img.encodePng(sepiaImg, level: 4));
       processed = _processedImg;
     }
+  }
+
+  Future<File> _docScanner(String originalImgPath) async {
+    // DORMANT: Prefer local image processing over DocScanner2 API call
+    const String _docScannerUrl =
+        'https://lastpage-docscanner2-poc-drsfalheva-km.a.run.app';
+    const String _docScannerEndPoint = '/v1/docscanner';
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+
+    // Set auth headers and send POST request
+    final String _authToken = await _auth.currentUser!.getIdToken();
+    var uri = Uri.parse(_docScannerUrl + _docScannerEndPoint);
+
+    var request = http.MultipartRequest('POST', uri);
+    request.headers.addAll({
+      "Authorization": "Bearer $_authToken",
+    });
+    request.files
+        .add(await http.MultipartFile.fromPath('file', originalImgPath));
+    var response = await request.send();
+
+    // if (response.statusCode != 200) {
+    //   print('DocScanner2 API returned status: ${response.statusCode}');
+    // }
+
+    // Handle API response and post-processing
+
+    final _scannedDoc = await _tempFile;
+    final Uint8List _scannedDocRaw = await response.stream.toBytes();
+
+    // Convert byte-data into image
+
+    img.Image? _receivedImg = img.decodeImage(_scannedDocRaw);
+    if (_receivedImg != null) {
+      // Reorient image based on EXIF tags (if available) on decoded image
+
+      _receivedImg = img.bakeOrientation(_receivedImg);
+
+      // Assuming image is always a portrait (paper), rotate image by 90 degrees
+      // if height is less than width and write image to temporary file (cache)
+      // TODO: [ENHANCE] Allow user to rotate and flip image
+
+      if (_receivedImg.height < _receivedImg.width) {
+        _receivedImg = img.copyRotate(_receivedImg, 90);
+      }
+      _scannedDoc.writeAsBytes(img.encodePng(_receivedImg));
+    } else {
+      _scannedDoc.writeAsBytes(_scannedDocRaw);
+    }
+    return _scannedDoc;
   }
 }
--- lib/models/pages.dart
+++ lib/models/pages.dart
@@ -1,11 +1,6 @@
 import 'dart:io';
-import 'dart:typed_data';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/widgets.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:http/http.dart' as http;
-import 'package:path_provider/path_provider.dart';
-import 'package:image/image.dart' as img;
 
 import './page.dart' as page;
 
@@ -14,16 +9,11 @@ class Pages with ChangeNotifier {
   // device camera and stores them in a list which is made available through
   // Provider. Page class stores the actual and enhanced images.
 
-// TODO: [ENHANCE] Fetch scanner URL and endpoint from DB
-  final String _docScannerUrl =
-      'https://lastpage-docscanner2-poc-drsfalheva-km.a.run.app';
-  final String _docScannerEndPoint = '/v1/docscanner';
-  final FirebaseAuth _auth = FirebaseAuth.instance;
   List<page.Page> allPages = [];
 
   capturePage() async {
-    // Separate try-catch blocks for camera and DocScanner to allow displaying
-    // camera image in case DocScanner API call fails
+    // Separate try-catch blocks for camera and image processing to allow displaying
+    // camera image while image is processed
     File originalPage;
     page.Page newPage;
     try {
@@ -36,27 +26,12 @@ class Pages with ChangeNotifier {
       rethrow;
     }
     try {
-      // var scannedPage = await _docScanner(newPage.original.path);
-      // newPage.processed = scannedPage;
       await newPage.processImg();
     } catch (e) {
       rethrow;
     } finally {
       notifyListeners();
     }
-  }
-
-  Future<String> get _tempPath async {
-    final directory = await getTemporaryDirectory();
-
-    return directory.path;
-  }
-
-  Future<File> get _tempFile async {
-    final path = await _tempPath;
-    final uniqueFileName = UniqueKey().toString();
-
-    return File('$path/$uniqueFileName.img');
   }
 
   Future<File> _camCapture() async {
@@ -68,51 +43,6 @@ class Pages with ChangeNotifier {
       preferredCameraDevice: CameraDevice.rear,
       imageQuality: 75,
     );
-    // TODO: Change orientation of image if height < width
     return File(capturedPage!.path);
-  }
-
-  Future<File> _docScanner(String originalImgPath) async {
-    // Set auth headers and send POST request
-    final String _authToken = await _auth.currentUser!.getIdToken();
-    var uri = Uri.parse(_docScannerUrl + _docScannerEndPoint);
-
-    var request = http.MultipartRequest('POST', uri);
-    request.headers.addAll({
-      "Authorization": "Bearer $_authToken",
-    });
-    request.files
-        .add(await http.MultipartFile.fromPath('file', originalImgPath));
-    var response = await request.send();
-
-    // if (response.statusCode != 200) {
-    //   print('DocScanner2 API returned status: ${response.statusCode}');
-    // }
-
-    // Handle API response and post-processing
-
-    final _scannedDoc = await _tempFile;
-    final Uint8List _scannedDocRaw = await response.stream.toBytes();
-
-    // Convert byte-data into image
-
-    img.Image? _receivedImg = img.decodeImage(_scannedDocRaw);
-    if (_receivedImg != null) {
-      // Reorient image based on EXIF tags (if available) on decoded image
-
-      _receivedImg = img.bakeOrientation(_receivedImg);
-
-      // Assuming image is always a portrait (paper), rotate image by 90 degrees
-      // if height is less than width and write image to temporary file (cache)
-      // TODO: [ENHANCE] Allow user to rotate and flip image
-
-      if (_receivedImg.height < _receivedImg.width) {
-        _receivedImg = img.copyRotate(_receivedImg, 90);
-      }
-      _scannedDoc.writeAsBytes(img.encodePng(_receivedImg));
-    } else {
-      _scannedDoc.writeAsBytes(_scannedDocRaw);
-    }
-    return _scannedDoc;
   }
 }
