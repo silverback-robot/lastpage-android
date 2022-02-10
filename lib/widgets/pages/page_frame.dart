bb3a1179a54df9a8487fd30e5a6cc8b2b2341560
yesharish <yesharish@yahoo.in>
2022-02-10 20:45:29 +1100

    UI adjustments for Pages view

--- lib/widgets/pages/page_frame.dart
+++ lib/widgets/pages/page_frame.dart
@@ -27,53 +27,76 @@ class _PageFrameState extends State<PageFrame> {
   @override
   Widget build(BuildContext context) {
     if (widget.page.processed != null) {
-      return Column(
-        children: [
-          SizedBox(
-            height: 400,
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Image.file(
-                _selectEnhanced ? widget.page.processed! : widget.page.original,
-              ),
+      return Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                Image.file(
+                  _selectEnhanced
+                      ? widget.page.processed!
+                      : widget.page.original,
+                  width: double.infinity,
+                ),
+
+                const SizedBox(
+                  height: 8,
+                ),
+                // const SizedBox(height: 12),
+                Container(
+                  height: 50,
+                  alignment: Alignment.bottomCenter,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: <Color>[
+                        Colors.black.withAlpha(0),
+                        Colors.black45,
+                        Colors.black
+                      ],
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton.icon(
+                        onPressed: _toggleSelection,
+                        icon: Icon(
+                          _selectEnhanced
+                              ? Icons.toggle_on
+                              : Icons.toggle_off_outlined,
+                        ),
+                        label: Text(
+                          _selectEnhanced ? "Enhanced" : "Original",
+                        ),
+                      ),
+                      TextButton.icon(
+                        onPressed: () => widget.deleteCallback(widget.page.key),
+                        icon: const Icon(Icons.delete),
+                        label: const Text("Delete"),
+                      ),
+                      TextButton.icon(
+                        onPressed: () async {
+                          await widget.page.cropImage();
+                          await widget.page.processImg();
+                        },
+                        icon: const Icon(Icons.edit),
+                        label: const Text("Edit"),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            ElevatedButton.icon(
-              onPressed: _toggleSelection,
-              icon: Icon(
-                _selectEnhanced ? Icons.toggle_on : Icons.toggle_off_outlined,
-              ),
-              label: Text(
-                _selectEnhanced ? "Enhanced" : "Original",
-              ),
-            ),
-            const SizedBox(
-              width: 8,
-            ),
-            ElevatedButton.icon(
-              onPressed: () => widget.deleteCallback(widget.page.key),
-              icon: const Icon(Icons.delete),
-              label: const Text("Delete Page"),
-            ),
-            const SizedBox(
-              width: 8,
-            ),
-            ElevatedButton.icon(
-              onPressed: () async {
-                await widget.page.cropImage();
-                await widget.page.processImg();
-              },
-              icon: const Icon(Icons.edit),
-              label: const Text("Edit"),
-            ),
-          ]),
-        ],
+        ),
       );
     } else {
       return SizedBox(
